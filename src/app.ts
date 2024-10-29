8211e8cc5bd624f1b0e8ba8dfa85928af9bee4af
John Grosh <dev.beale@therockhillgroup.com>
2024-10-29 12:11:21 -0700

    WS Echo

--- src/app.ts
+++ src/app.ts
@@ -53,6 +53,33 @@ process.on('uncaughtException', (error) => {
   process.exit(1); // exit with a 'failure' code
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+let WSServer = require('ws').Server;
+let server = require('http').createServer();
+
+// Create web socket server on top of a regular http server
+let wss = new WSServer({
+
+  server: server
+});
+
+// Also mount the app here
+server.on('request', app);
+
+wss.on('connection', function connection(ws : any) {
+ 
+  ws.on('message', function incoming(message : any) {
+    
+    console.log(`received: ${message}`);
+    
+    ws.send(JSON.stringify({
+
+      answer: 42
+    }));
+  });
+});
+
+
+server.listen(3000, function() {
+
+  console.log(`http/ws server listening on ${3000}`);
 });
